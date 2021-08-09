79b956cc9f02bfb310ec30acc544dee4ff450006
Yaemta <yaemta@gmail.com>
2021-08-09 20:55:05 +0300

    initial commit

--- src/model/Course.java
+++ src/model/Course.java
@@ -1,4 +1,90 @@
 package model;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String courseName;
+    private String courseCode;
+    private int creditScore;
+
+    @ManyToOne
+    private Instructor instructor;
+    @ManyToMany
+    private List<Student> studentList = new ArrayList<>();
+
+    public Course(String courseName, String courseCode, int creditScore, Instructor instructor) {
+
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.creditScore = creditScore;
+        this.instructor = instructor;
+    }
+
+    public Course(String courseName, String courseCode, int creditScore) {
+        this.courseName = courseName;
+        this.courseCode = courseCode;
+        this.creditScore = creditScore;
+    }
+
+    public Course() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getCreditScore() {
+        return creditScore;
+    }
+
+    public void setCreditScore(int creditScore) {
+        this.creditScore = creditScore;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    @Override
+    public String toString() {
+        return courseName ;
+    }
 }
